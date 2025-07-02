0983d19fb855289fb867c32ffc7e205e13bccfdd
Mustafa Hasanain <m7asanain@gmail.com>
2025-07-02 11:56:47 +0300

    feat(navbar): implement responsive navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">Hello GSAP</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
