77890a52fb9f144677d1313fb1cfe85118f61007
Mustafa Hasanain <m7asanain@gmail.com>
2025-07-02 14:05:55 +0300

    implement animation for the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,55 @@
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+
 const Hero = () => {
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", { type: "chars, words" });
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.05,
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "button top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+  }, []);
+
   return (
     <>
       <section id="hero" className="noisy">
         <h1 className="title">MOJITO</h1>
         <img
           src="/images/hero-left-leaf.png"
-          alt="hero-left-leaf"
+          alt="left-leaf"
           className="left-leaf"
         />
-
         <img
           src="/images/hero-right-leaf.png"
-          alt="hero-right-leaf"
+          alt="right-leaf"
           className="right-leaf"
         />
 
